793722e677877f4d60f9cbb10f06af3066af7abf
崔新姝 <cuixinshu@cuixinshudeMacBook-Pro.local>
2020-11-12 22:41:09 +0800

    绘制登录页面 v0.1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="app">
-    App
+    <!-- 路由占位符 -->
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
 
 <style>
 </style>
