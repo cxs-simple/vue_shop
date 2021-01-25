8b8f21ca08972677f67edff181bee3892ceed936
崔新姝 <cuixinshu@cuixinshudeMacBook-Pro.local>
2021-01-25 22:14:55 +0800

    绘制主页页面 实现基本主页布局 v0.1

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <!-- 侧边栏区域 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+          <!-- 一级菜单区域 -->
+          <el-submenu index="1">
+            <!-- 一级菜单模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单区域 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主体区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -19,5 +55,36 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
 
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    img {
+      height: 100%;
+    }
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
-import { Form, FormItem, Input, Button, Message } from 'element-ui'
+import { Form, FormItem, Input, Button, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem } from 'element-ui'
 
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
 // Message全局弹框组件挂载到Vue全局中
 Vue.prototype.$message = Message
