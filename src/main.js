d1c6fd0802e59690e9dd878511784e78e7b15f7b
崔新姝 <cuixinshu@cuixinshudeMacBook-Pro.local>
2021-01-27 21:58:26 +0800

    绘制主页页面 通过axios请求拦截器添加token v0.2

--- src/main.js
+++ src/main.js
@@ -10,6 +10,15 @@ import axios from 'axios'
 // 配置请求的跟路径
 // axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+
+// 通过axios请求拦截器添加token，保证拥有获取数据的权限
+axios.interceptors.request.use(config => {
+  // 为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 固定写法： 必须返回config(config为一次请求对象)
+  return config
+})
+
 // 将axios挂载Vue中
 Vue.prototype.$http = axios
 
