45f1e58070c080ad2850a9974e59d92bec64184e
崔新姝 <cuixinshu@cuixinshudeMacBook-Pro.local>
2021-01-30 13:54:56 +0800

    绘制主页 v1.0

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,38 +10,63 @@
     </el-header>
     <el-container>
       <!-- 侧边栏区域 -->
-      <el-aside width="200px">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu background-color="#545c64" text-color="#fff" active-text-color="#409EFF" unique-opened
+                 :collapse="isCollapse" :collapse-transition="false" router="">
           <!-- 一级菜单区域 -->
-          <el-submenu index="1">
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
             <!-- 一级菜单模板区域 -->
             <template slot="title">
               <!-- 图标 -->
-              <i class="el-icon-location"></i>
+              <i :class="iconObjts[item.id]"></i>
               <!-- 文本 -->
-              <span>导航一</span>
+              <span>{{ item.authName }}</span>
             </template>
             <!-- 二级菜单区域 -->
-            <el-menu-item index="1-4-1">
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id">
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-location"></i>
                 <!-- 文本 -->
-                <span>导航一</span>
+                <span>{{ subItem.authName }}</span>
               </template>
             </el-menu-item>
           </el-submenu>
         </el-menu>
       </el-aside>
       <!-- 主体区域 -->
-      <el-main>Main</el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 左侧菜单栏数据
+      menuList: [],
+      // 左侧菜单栏图标字典
+      iconObjts: {
+        125: 'iconfont iconusers',
+        103: 'iconfont iconquanxian',
+        101: 'iconfont iconshangpin',
+        102: 'iconfont icondingdan',
+        145: 'iconfont iconbaobiao'
+      },
+      // 左侧菜单栏折叠与展开
+      isCollapse: false
+    }
+  },
+
+  created () {
+    this.getMenuList()
+  },
+
   methods: {
     // 退出
     logout () {
@@ -49,6 +74,19 @@ export default {
       window.sessionStorage.clear()
       // 编程式导航跳转到登录页面
       this.$router.push('/login')
+    },
+
+    // 获取菜单栏列表
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$Message.error('')
+      this.menuList = res.data
+      console.log(res)
+    },
+
+    // 左侧菜单栏折叠与展开
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
@@ -82,9 +120,26 @@ export default {
 
 .el-aside {
   background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
 }
 
 .el-main {
   background-color: #eaedf1;
+}
+
+.iconfont {
+  margin-right: 10px;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 15px;
+  text-align: center;
+  color: #fff;
+  letter-spacing: 0.2em;
+  line-height: 25px;
+  cursor: pointer;
 }
 </style>
